b92abc2c1117ec300d83713b53a2f5b91796986b
kate1505 <b.katrin.v@gmail.com>
2023-02-27 22:54:56 +0300

    add src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,21 @@
+import runGame from "../index.js";
+import getRandomInt from "../randomNumber.js";
+
+const description = "Find the greatest common divisor of given numbers.";
+
+const getNod = (x, y) => {
+  if (y > x) return getNod(y, x);
+  if (!y) return x;
+  return getNod(y, x % y);
+};
+
+const getRoundData = () => {
+  const x = getRandomInt(10);
+  const y = getRandomInt(10);
+  const question = `${x} ${y}`;
+  const rightAnswer = String(getNod(x, y));
+  return [question, rightAnswer];
+};
+
+const gcdGame = () => runGame(description, getRoundData);
+export default gcdGame;
