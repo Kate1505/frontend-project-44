25cb29984de9832926048ae608cc9184429deb26
kate1505 <b.katrin.v@gmail.com>
2023-02-28 15:49:46 +0300

    add src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,29 @@
+import runGame from "../index.js";
+import getRandomInt from "../randomNumber.js";
+
+const description = "What number is missing in the progression?";
+
+const getProgression = (num1, step, arrProgresLength) => {
+  const arrProgressive = [];
+  for (let i = 1; i < arrProgresLength; i += 1) {
+    const result = num1 + step * i;
+    arrProgressive.push(result);
+  }
+  return arrProgressive;
+};
+
+const getRoundData = () => {
+  const arrProgresLength = Math.floor(
+    Math.random() * (Math.floor(10) - Math.ceil(5) + 1) + Math.ceil(5)
+  );
+  const num1 = getRandomInt(10);
+  const step = getRandomInt(10);
+  const progression = getProgression(num1, step, arrProgresLength);
+  const randomIndex = getRandomInt(9);
+  const rightAnswer = `${progression[randomIndex]}`;
+  progression[randomIndex] = "..";
+  const question = progression.join(" ");
+  return [question, rightAnswer];
+};
+const progressionGame = () => runGame(description, getRoundData);
+export default progressionGame;
