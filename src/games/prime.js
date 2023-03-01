6ae042efce72e0c6aeff750dad867c315e5bd740
kate1505 <b.katrin.v@gmail.com>
2023-03-01 16:43:56 +0300

    update src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,8 +6,8 @@ const description =
 
 const getPrime = (number) => {
   if (number < 2) return false;
-  for (let i = 2; i < number / 2; i++) {
-    if (number % i == 0) {
+  for (let i = 2; i < number / 2; i += 1) {
+    if (number % i === 0) {
       return false;
     }
   }
