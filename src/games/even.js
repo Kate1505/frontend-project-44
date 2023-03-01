feb80ea08a5bc08ab7be707a4b2eb657af6b1ecd
kate1505 <b.katrin.v@gmail.com>
2023-03-01 17:38:37 +0300

    update

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ const evenGame = () => {
   const description =
     'Answer "yes" if the number is even, otherwise answer "no".';
   const getRoundData = () => {
-    const randomNumber = getRandomInt(100);
+    const randomNumber = getRandomInt(1, 100);
     const rightAnswer = randomNumber % 2 === 0 ? "yes" : "no";
     return [randomNumber, rightAnswer];
   };
