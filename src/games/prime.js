b47e1d21d6b1a26c339c07b8f45e86acbd78cace
kate1505 <b.katrin.v@gmail.com>
2023-03-02 11:29:35 +0300

    fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const getPrime = (number) => {
   if (number < 2) return false;
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
     }
