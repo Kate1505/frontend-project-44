958d63b58b75d10f470f45e2715ba3c26560f5e0
kate1505 <b.katrin.v@gmail.com>
2023-03-04 13:31:22 +0300

    fix bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
 
-import nameUser from '../src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-const greetingUser = () => {
-  nameUser();
-};
+import greetingUser from '../src/cli.js';
 
 greetingUser();
