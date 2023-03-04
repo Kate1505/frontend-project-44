c76fbbfcc8472737aa3d61ddaf8bc722029017b9
kate1505 <b.katrin.v@gmail.com>
2023-03-04 13:45:38 +0300

    fix src/games/even.js

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,10 @@
 import runGame from '../index.js';
-import getRandomInt from '../randomNumber.js';
+import getRandomNumber from '../lib.js';
 
 const evenGame = () => {
   const description = 'Answer "yes" if the number is even, otherwise answer "no".';
   const getRoundData = () => {
-    const randomNumber = getRandomInt(1, 100);
+    const randomNumber = getRandomNumber(1, 100);
     const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
     return [randomNumber, rightAnswer];
   };
