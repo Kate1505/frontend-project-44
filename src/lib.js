5e1917867fba697947bf68421eaeeca33bb15c90
kate1505 <b.katrin.v@gmail.com>
2023-03-12 18:29:49 +0300

    fix  src/lib.js

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,7 @@
 const getRandomNumber = (min, max) => {
   const minNumber = Math.ceil(min);
   const maxNumber = Math.floor(max);
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+  const randomInt = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+  return randomInt;
 };
 export default getRandomNumber;
